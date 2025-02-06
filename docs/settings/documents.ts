e1efda5ffbcb955ef77e28bb874494418dc2ef9e
SSE World <suryakantmaurya2007@gmail.com>
2025-02-06 20:58:43 +0530

    feat: add initial documentation structure with configuration and components

---
+++ docs/settings/documents.ts
@@ -0,0 +1,134 @@
+import { Paths } from "@/lib/pageroutes"
+
+export const Documents: Paths[] = [
+  {
+    title: "Introduction",
+    href: "/introduction",
+    heading: "Getting started",
+    items: [
+      {
+        title: "Installation",
+        href: "/installation",
+      },
+      {
+        title: "Setup",
+        href: "/setup",
+      },
+      {
+        title: "Changelog",
+        href: "/changelog",
+      },
+    ],
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Libraries",
+    href: "/lib",
+    items: [
+      {
+        title: "Atob",
+        href: "/atob"
+      },
+      {
+        title: "Btoa",
+        href: "/btoa"
+      },
+      {
+        title: "Base64",
+        href: "/base64"
+      },
+      {
+        title: "Blob",
+        href: "/blob"
+      },
+      {
+        title: "Buffer",
+        href: "/buffer"
+      },
+      {
+        title: "File",
+        href: "/file"
+      },
+      {
+        title: "Hash",
+        href: "/hash"
+      },
+      {
+        title: "Hmac",
+        href: "/hmac"
+      },
+      {
+        title: "ieee754",
+        href: "/ieee754"
+      },
+      {
+        title: "MS",
+        href: "/ms"
+      },
+      {
+        title: "Punycode",
+        href: "/punycode"
+      },
+      {
+        title: "Querystring",
+        href: "/qs"
+      },
+      {
+        title: "Bcrypt",
+        href: "/bcrypt"
+      },
+      {
+        title: "Base32",
+        href: "/base32"
+      },
+    ]
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Markdown",
+    href: "/markdown",
+    heading: "Components",
+    items: [
+      {
+        title: "Cards",
+        href: "/cards",
+      },
+      {
+        title: "Diagrams",
+        href: "/diagrams",
+      },
+      {
+        title: "Filetree",
+        href: "/filetree",
+      },
+      {
+        title: "Lists",
+        href: "/lists",
+      },
+      {
+        title: "Maths",
+        href: "/maths",
+      },
+      {
+        title: "Notes",
+        href: "/notes",
+      },
+      {
+        title: "Steps",
+        href: "/steps",
+      },
+      {
+        title: "Table",
+        href: "/table",
+      },
+      {
+        title: "Tabs",
+        href: "/tabs",
+      },
+    ],
+  },
+]
